d8cd2f95916ca353be986bd47a6409bc2f95bf1c
Abdullah Al Noman <abdnoman720@gmail.com>
2026-03-27 14:21:15 +0600

    Hero Section Done

--- nike/src/components/Button.jsx
+++ nike/src/components/Button.jsx
@@ -1,8 +1,14 @@
-const Button = () => {
+const Button = ({label, iconURL}) => {
     return (
-        <div>
-            
-        </div>
+        <button className="flex justify-center items-center gap-2
+        px-7 py-4 border font-montserrat text-lg leading-none
+        bg-coral-red rounded-full text-white">
+            {label}
+            <img
+                src={iconURL}
+                alt="arrow right icon"
+            className="ml-2 rounded-full w-5 h-5"/>
+        </button>
     );
 };
 
